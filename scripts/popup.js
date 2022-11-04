38bbdd3ab2811485f49cc7f997964425d6698e1d
vincentT360 <vincentthaiasij@gmail.com>
2022-11-03 17:10:52 -0700

    Added bootstrap support and button request

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,7 +4,7 @@ function delay(time){
   });
 }
 
-async function sendRequest(){
+async function sendSingleRequest(){
   try{
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await res.json();
@@ -14,6 +14,15 @@ async function sendRequest(){
   }
 }
 
-sendRequest();
-await delay(5000);
-sendRequest();
+async function sendRequest(){
+  await sendSingleRequest();
+  //const start = new Date(); // Used to debug time elapsed
+  await delay(5000);
+  //const end = new Date();
+  //console.log(start - end)
+  await sendSingleRequest();
+}
+
+//Note: We have to do getElementById b/c chrome does not allow inline scripting like onclick=myfunction() in HTML
+//Basically, all scripting needs to stay in the js file
+document.getElementById("sendRequest").addEventListener("click", sendRequest);
