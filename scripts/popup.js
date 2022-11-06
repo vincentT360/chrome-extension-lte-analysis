a7b0ed291695eca8e82453a20dc200052bfc9516
vincentT360 <vincentthaiasij@gmail.com>
2022-11-06 12:42:15 -0800

    Initial websockets

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,25 +4,35 @@ function delay(time){
   });
 }
 
-async function sendSingleRequest(){
-  try{
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
-    console.log(data);
-  } catch(error){
-    console.error(`ERROR: ${error}`)
-  }
+//IP addr for capturing packets:
+//https://www.nslookup.io/domains/https%3A%2F%2Fsocketsbay.com%2Ftest-websockets/webservers/
+
+//Can also test what we are sending by going here and pressing connect, then in our extension send requests
+//https://socketsbay.com/test-websockets
+const socket = new WebSocket('wss://socketsbay.com/wss/v2/2/demo/')
+function connect(){
+    return new Promise(function(resolve, reject) {
+      var server = new WebSocket('wss://socketsbay.com/wss/v2/2/demo/')
+      server.onopen = function() {
+        console.log("success")
+        resolve(server);
+      };
+      server.onerror = function(err) {
+        reject(err)
+      }
+    })
+
 }
 
-async function sendRequest(){
-  await sendSingleRequest();
-  //const start = new Date(); // Used to debug time elapsed
+//By putting this outside of a function, when extension is opened via popup, we establish connection first
+let server = await connect();
+
+async function sendWebSocket(){
+  socket.send("hi1")
   await delay(5000);
-  //const end = new Date();
-  //console.log(start - end)
-  await sendSingleRequest();
+  socket.send("hi2")
 }
 
 //Note: We have to do getElementById b/c chrome does not allow inline scripting like onclick=myfunction() in HTML
 //Basically, all scripting needs to stay in the js file
-document.getElementById("sendRequest").addEventListener("click", sendRequest);
+document.getElementById("sendRequest").addEventListener("click", sendWebSocket);
