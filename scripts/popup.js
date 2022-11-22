9326fac938f87f0ede1555263e3ceb2f5325e78d
vincentT360 <vincentthaiasij@gmail.com>
2022-11-21 19:48:54 -0800

    Added multiple request sending and table view of results

--- scripts/popup.js
+++ scripts/popup.js
@@ -7,17 +7,8 @@ function wait(ms){
   }
 }
 
-//IP addr for capturing packets:
-//https://www.nslookup.io/domains/https%3A%2F%2Fsocketsbay.com%2Ftest-websockets/webservers/
-
-//Can also test what we are sending by going here and pressing connect, then in our extension send requests
-//Our packets sent will pop up in the website below
-//https://socketsbay.com/test-websockets
-// const socket = new WebSocket('wss://socketsbay.com/wss/v2/2/demo/')
-
 function connect(){
   return new Promise(function(resolve, reject){
-    //var server = new WebSocket('wss://socketsbay.com/wss/v2/2/demo/');
     var server = new WebSocket('ws://131.179.176.31:8080');
     server.onopen = function(){
       console.log("WebSocket connection established");
@@ -31,32 +22,124 @@ function connect(){
   });
 }
 
-function sendWebSocket(){
-  var gap = Number(document.getElementById("timeGap").value)
+//Basic building block to send one request
+function sendWebSocket(gap){
   
   if(connectionEstablished){
-    console.log(`Sending request 1 at: ${Date.now()} with gap of ${gap}`);
+
     server.send("rq 1");
+    var time0 = performance.now()
+    console.log(time0)
     wait(gap);
-    console.log(`Sending request 2 at: ${Date.now()}`);
+    var time1 = performance.now()
+    console.log(time1)
     server.send("rq 2");
+
   }
   else{
     console.log("Could not send requests because the WebSocket connection failed");
   }
 }
 
+//This function sends multiple send requests
+function sendMultiple() {
+
+  var gap = Number(document.getElementById("timeGap").value);
+  resetReceivedTracking();
+  
+  //Change the number in the for loop to how many times you want to send
+  for(let i = 0; i < NUMSENDS; i++) {
+    sendWebSocket(gap);
+    wait(100);
+  }
+
+}
+
+//Reset our global variables
+function resetReceivedTracking() {
+  totalReceivedMessages = 0;
+  receivedGaps = new Map();
+  receivedGapsString = "";
+  document.getElementById("receivedGap").innerHTML = "";
+}
+
+//Generate a table to display to report the receiving gaps
+function reportGaps() {
+  const sortedTimes = new Map([...receivedGaps].sort((a,b) => a[0] - b[0]));
+
+  const tbl = document.createElement("table");
+  tbl.setAttribute("class", "table");
+  const tblHead = document.createElement("thead");
+  const tblBody = document.createElement("tbody");
+
+  const tblHeadRow = document.createElement("tr");
+  const tblHeadCell1 = document.createElement("td");
+  const tblHeadCell2 = document.createElement("td");
+  const tblHeadCellText1 = document.createTextNode("Time");
+  const tblHeadCellText2 = document.createTextNode("Count");
+
+  tblHeadCell1.appendChild(tblHeadCellText1);
+  tblHeadCell2.appendChild(tblHeadCellText2);
+  tblHeadRow.appendChild(tblHeadCell1);
+  tblHeadRow.appendChild(tblHeadCell2);
+  tblHead.appendChild(tblHeadRow);
+      
+  for (let [key, value] of sortedTimes) {
+
+    const row = document.createElement("tr");
+    const cellTime = document.createElement("td");
+    const cellTimeText = document.createTextNode(key);
+    cellTime.appendChild(cellTimeText);
+    row.appendChild(cellTime);
+
+    const cellCount = document.createElement("td");
+    const cellCountText = document.createTextNode(value);
+    cellCount.appendChild(cellCountText);
+    row.appendChild(cellCount);
+    tblBody.appendChild(row);
+
+  }
+
+  tbl.appendChild(tblHead);
+  tbl.appendChild(tblBody);
+  document.getElementById("receivedGap").appendChild(tbl);
+
+}
+
+//Below global variables are used to keep track of sends
+var totalReceivedMessages = 0;
+var receivedGaps = new Map();
+var receivedGapsString = "";
+const NUMSENDS = 10;
+
 //By putting this outside of a function, when extension is opened via popup, we establish connection first
 try {
   var connectionEstablished = false;
   var server = await connect();
-  document.getElementById("sendRequest").addEventListener("click", sendWebSocket);
+
+  document.getElementById("sendRequest").addEventListener("click", sendMultiple);
 
   //This fires when we get a message back from the server
   server.onmessage = (event) => {
-  console.log(`Reported received time interval ${event.data}ms`)
-  document.getElementById("receivedGap").innerHTML = `Received Gap: ${event.data}ms`
-}
+
+    //This calculates the received gap into .25 increments and puts them into buckets
+    //Used to see what our distribution looks like.
+    var receivedGap = Number(event.data)
+    var rounded = (Math.round(receivedGap * 4) / 4).toFixed(2);
+
+    if (receivedGaps.has(rounded)) {
+      receivedGaps.set(rounded, receivedGaps.get(rounded) + 1);
+    } else {
+      receivedGaps.set(rounded, 1);
+    }
+
+    totalReceivedMessages += 1
+    if (totalReceivedMessages == NUMSENDS) {
+      reportGaps();
+    }
+  }
+  
 } catch(error) {
   document.getElementById("errorMessage").innerHTML = "Can't establish connection"
+  console.log(err)
 }
